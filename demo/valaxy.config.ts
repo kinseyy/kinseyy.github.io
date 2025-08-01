2d7af0001e49246fa05075622cbd13e8cc780aaf
kinsey973 <2409101203@qq.com>
2025-08-01 12:59:14 +0800

    completed-1

--- demo/valaxy.config.ts
+++ demo/valaxy.config.ts
@@ -2,7 +2,7 @@
  * @Author: kinsey973 2409101203@qq.com
  * @Date: 2025-08-01 11:59:05
  * @LastEditors: kinsey973 2409101203@qq.com
- * @LastEditTime: 2025-08-01 12:06:04
+ * @LastEditTime: 2025-08-01 12:53:13
  * @FilePath: \valaxy-blog\demo\valaxy.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
